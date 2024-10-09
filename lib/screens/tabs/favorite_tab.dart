d56b7ebae5d0b7000a8a05f9fa26a5bb30110827
Lans12345 <olanalans12345@gmail.com>
2024-10-10 06:46:34 +0800

    added favorites tab ui

--- lib/screens/tabs/favorite_tab.dart
+++ lib/screens/tabs/favorite_tab.dart
@@ -21,6 +21,90 @@ class FavoriteTab extends StatelessWidget {
               ),
             ),
           ),
+          Wrap(
+            children: [
+              for (int i = 0; i < 7; i++)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.grey[300],
+                    child: SizedBox(
+                      width: 180,
+                      height: 265,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Align(
+                              alignment: Alignment.topRight,
+                              child: Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                                size: 30,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Center(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                width: 150,
+                                height: 125,
+                                child: Center(
+                                  child: Image.asset(
+                                    'assets/images/Web_Photo_Editor 1.png',
+                                    width: 125,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextWidget(
+                              text: 'AquaLinda',
+                              fontSize: 18,
+                              fontFamily: 'Bold',
+                              color: Colors.black,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Center(
+                              child: GestureDetector(
+                                child: Container(
+                                  width: 125,
+                                  height: 35,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      border: Border.all(
+                                        color: Colors.grey,
+                                      )),
+                                  child: Center(
+                                    child: TextWidget(
+                                      text: 'Add to cart',
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                      fontFamily: 'Bold',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ],
       ),
     );
